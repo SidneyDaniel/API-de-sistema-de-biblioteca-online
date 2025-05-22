29f5866a0d3d1956ecf325feb336449da6201505
Sid.Daniel <sidneylopes17@gmail.com>
2025-05-22 13:34:54 -0300

    ✅ | test |  Testes para validar funcionalidades da aplicação e rotas da API
    Testes de integração para garantir que as funcionalidades principais da aplicação funcionem como esperado. Adicionados testes para gerenciamento de usuários (cadastro, edição, exclusão) e gerenciamento de livros (adicionar, editar, excluir).

---
+++ server/cypress/fixtures/book.ts
@@ -0,0 +1,12 @@
+export const bookData = {
+  "title": {
+    "current": "Ultimate Web Automation Testing with Cypress: Master End-to-End Web Application Testing Automation to Accelerate Your QA Process with Cypress (English Edition)",
+    "new": "Ultimate Web Automation Testing with Cypress"
+  },
+  "author": "Vitaly Skadorva",
+  "publisher": "Orange Education Pvt Ltd",
+  "pages": "547",
+  "image": "https://...jpg",
+  "link": "https://www.amazon.com.br/..."
+}
+
